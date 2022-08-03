638dacce8bbac40a2aeefd28e18aefecbeac005c
Samer A <thedev.samer@gmail.com>
2022-08-03 17:11:46 +0300

    built a reusable icon component

--- App.js
+++ App.js
@@ -7,9 +7,15 @@ import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
 
 const App = () => {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+    </Screen>
+  );
 };
 
 export default App;
---
+++ app/components/Icon.js
@@ -0,0 +1,27 @@
+import { View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../config/colors";
+
+const Icon = ({
+  name,
+  size = 40,
+  backgroundColor = colors.black,
+  iconColor = colors.white,
+}) => {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        backgroundColor,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <MaterialCommunityIcons name={name} size={size * 0.6} color={iconColor} />
+    </View>
+  );
+};
+
+export default Icon;
