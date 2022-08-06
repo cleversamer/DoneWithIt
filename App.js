c57e443da29ce52ebc56695ce72e4d3d35a30fa0
Samer A <thedev.samer@gmail.com>
2022-08-06 12:40:10 +0300

    finished building the app navigator

--- App.js
+++ App.js
@@ -1,60 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 
 const App = () => {
-  const Stack = createNativeStackNavigator();
-  const Tab = createBottomTabNavigator();
   const screenOptions = {
     headerShown: false,
     contentStyle: { backgroundColor: "transparent" },
   };
 
-  const StackNavigator = () => (
-    <Stack.Navigator screenOptions={screenOptions}>
-      <Stack.Screen name="Listing Edit" component={ListingEditScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-    </Stack.Navigator>
-  );
-
-  const TabNavigator = () => (
-    <Tab.Navigator
-      screenOptions={{
-        ...screenOptions,
-        tabBarActiveBackgroundColor: "tomato",
-        tabBarActiveTintColor: "#fff",
-      }}
-    >
-      <Tab.Screen
-        name="Items"
-        component={ListingEditScreen}
-        options={{
-          tabBarIcon: (props) => (
-            <MaterialCommunityIcons name="email" {...props} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{
-          tabBarIcon: (props) => (
-            <MaterialCommunityIcons name="email" {...props} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-
   return (
     <NavigationContainer>
-      <AuthNavigator screenOptions={screenOptions} />
+      <AppNavigator screenOptions={screenOptions} />
     </NavigationContainer>
   );
 };
---
+++ app/navigation/AppNavigator.js
@@ -0,0 +1,17 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import ListingsScreen from "../screens/ListingsScreen";
+import ListingEditScreen from "../screens/ListingEditScreen";
+import AccountScreen from "../screens/AccountScreen";
+
+const Tab = createBottomTabNavigator();
+
+const AppNavigator = ({ screenOptions }) => (
+  <Tab.Navigator screenOptions={screenOptions}>
+    <Tab.Screen name="Listings" component={ListingsScreen} />
+    <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
+    <Tab.Screen name="Account" component={AccountScreen} />
+  </Tab.Navigator>
+);
+
+export default AppNavigator;
